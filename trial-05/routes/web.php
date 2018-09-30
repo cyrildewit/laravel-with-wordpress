724daa6c31619c4d250bb4bce39065b10fb6580a
Cyril de Wit <cyrilwit@gmail.com>
2018-09-30 20:09:41 +0200

    refactor: clean up

--- trial-05/routes/web.php
+++ trial-05/routes/web.php
@@ -14,20 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get('/wordpress/{params?}', function ($params = null) {
-//     if (! $params) {
-//         $params = 'index';
-//     }
-
-//     $path = '../wordpress/' . $params.'.php';
-
-//     return $path;
-
-//     return require $path;
-// });
-
-
-Route::get('/wordpress/config', function () {
-    return require '../wordpress/wp-admin/setup-config.php';
-});
